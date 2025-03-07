dd33909c0ed5e56c2b8afb37bd6e672e6c2ef8fb
Mayor Lee <Adewoyemayowa411@gmail.com>
2025-03-07 06:13:46 -0800

    first commit

---
+++ javascripts/scripts/app.js
@@ -0,0 +1,286 @@
+// alert("Hello world!")
+console.log("Hello world!");
+document.getElementById("heading").innerHTML = "welcome <i>Felix</i>!"
+
+var lastname ="Uthman";
+var firstname ="null"
+var age = 100;
+var isyoung = false;
+var isyoung = true;
+
+console.log(lastname)
+console.log(age)
+console.log(firstname)
+
+var a = 20
+var b = a
+console.log(b)
+
+
+var b= 50
+var c=a+b 
+console.log(c)
+
+
+var a = 40
+var b = 20
+var c = a *b
+console.log(c)
+
+var a = 40
+var b = 20
+var c = a /b
+console.log(c)
+
+var a = 40
+var b = 20
+var c = a -b
+console.log(c)
+
+var inde= 50
+var bold =20
+var clas=20
+var done=20
+var f= inde*(bold+clas*done)
+console.log(f)
+
+var a=50
+var l= a+=20
+console.log(l)
+
+var a=50
+ a+=20
+console.log(a)
+
+a-=30
+console.log(a);
+
+a/=10
+console.log(a);
+
+a%=3
+console.log(a);
+
+a*=20
+console.log(a);
+
+a*=(50*50)
+console.log(a);
+
+
+var pie=3.142
+var r= 5
+console.log(pie*r**2);
+
+var pie=3.142
+var r=20
+console.log(pie*r**3);
+
+
+// Area of a triangle;
+
+
+var A= 20
+var base= 20
+var height =50
+var A=1/2*base*height
+console.log(A);
+
+// concatenation, prompt, Number, parseInt, parsefloat
+
+// concatenation example
+
+var firstname ="Adewoye"
+var lastname ="Mayowa"
+
+var a="30.8"
+var b= 20;
+
+var fullname= `${firstname} ${lastname} `
+var c = Number(a)+b;
+console.log(c);
+console.log(fullname);
+
+var firstname="Adewoye"
+var lastname="mayowa"
+var course="cybersecurity"
+var school="SQI"
+var bio= `my name is ${firstname} ${lastname} and i am a student of ${school} studying ${course}`
+console.log(bio);
+
+// var firstname = prompt("Enter your firstname")
+// var lastname = prompt("Enter your lastname")
+// var course = prompt("Enter your course")
+// var school = prompt("Enter your school")
+// console.log(firstname, lastname, course, school );
+
+
+
+
+
+
+
+// circumference of a circle
+var pie = 3.142
+var r= 7
+var c = 2*pie*r
+console.log(c);
+
+// Area of a trapezium 
+var base1 = 5 
+var base2 = 8 
+var h = 3 
+var A = 1/2*(base1+base2)*h 
+console.log(A)
+
+
+// Area of a parallelogram
+var b = 8
+var h = 5
+var Ap = b*h
+console.log(Ap);
+
+//Body mass index
+var w = 50
+var h = 2.0
+var bod = w/h
+console.log(bod);
+
+
+
+
+
+// ++  --
+
+var a = 10
+a++
+console.log(a);
+a++
+console.log(a);
+a--
+console.log(a);
+a--
+console.log(a);
+
+
+// if(condition){
+//     code to be excuted 
+// }
+// example:
+var g = 5
+var h = 5
+if(g==h){
+    console.log(true);
+    }
+    
+
+
+
+// else if(condition){
+//     code to be excuted 
+// }
+// Example:
+
+else{
+    console.log(true);
+ }
+
+
+
+ var y="8"
+ var r =8
+ if(y==r){
+    console.log(true);
+    }
+ else{
+    console.log(false);
+ }
+    
+ var y="8"
+ var r =8
+ if(y===r){
+    console.log(true);
+    }
+ else{
+    console.log(false);
+ }
+    
+ var f = 5
+ var k = 6
+
+ if(f==k)
+    console.log(true);
+else{console.log(false);
+}
+
+var f = 5
+ var k = 6
+
+ if(f===k)
+    console.log(true);
+else{ console.log(false);
+}
+// ============================
+var f = 5
+var k = 5
+
+if(f!=k){
+    console.log(true)
+}
+
+else{
+    console.log(false);
+}
+    // ========================
+var f = "5"
+var k = 5
+
+if(f!=k){
+    console.log(true)
+}
+
+else{
+    console.log(false);
+}
+
+// ===============
+if(f!==k){
+    console.log(true)
+}
+
+else{
+    console.log(false);
+}
+    
+
+
+
+
+
+
+// else{code to be excuted}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
